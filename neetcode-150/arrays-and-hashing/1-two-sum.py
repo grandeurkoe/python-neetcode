a940fca2aad0cfdebf6af05181d36ce329f429a5
Vishal Kushwaha <133014886+grandeurkoe@users.noreply.github.com>
2023-12-08 07:44:29 +0530

    Add 1-two-sum

---
+++ neetcode-150/arrays-and-hashing/1-two-sum.py
@@ -0,0 +1,15 @@
+# Two Sum
+# Leetcode Link - https://leetcode.com/problems/two-sum/
+
+class Solution:
+    def twoSum(self, nums, target):
+        for first_idx in range(len(nums)):
+            for second_idx in range(first_idx + 1, len(nums)):
+                if nums[first_idx] + nums[second_idx] == target:
+                    return [first_idx, second_idx]
+
+
+solution = Solution()
+nums = [3, 2, 4]
+target = 6
+print(f"\nTwo Sum: {solution.twoSum(nums, target)}\n")
